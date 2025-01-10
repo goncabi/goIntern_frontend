6ddfd937e60ef746772066a458c999e1ff5768f2
Beyza Nur Acikgoez <beyzanuracikgoez@gmail.com>
2025-01-10 16:53:11 +0100

    Korrektur: korrigierte/funktionierende Anwendung.

--- src/main/frontend/generated/theme-my-app.generated.js
+++ src/main/frontend/generated/theme-my-app.generated.js
@@ -6,8 +6,8 @@ let needsReloadOnChanges = false;
 import { typography } from '@vaadin/vaadin-lumo-styles/typography.js';
 import { color } from '@vaadin/vaadin-lumo-styles/color.js';
 import { spacing } from '@vaadin/vaadin-lumo-styles/spacing.js';
-import { badge } from '@vaadin/vaadin-lumo-styles/badge.js';
 import { utility } from '@vaadin/vaadin-lumo-styles/utility.js';
+import { badge } from '@vaadin/vaadin-lumo-styles/badge.js';
 
   let themeRemovers = new WeakMap();
   let targets = [];
@@ -18,8 +18,8 @@ import { utility } from '@vaadin/vaadin-lumo-styles/utility.js';
       removers.push(injectGlobalCss(typography.cssText, '', target, true));
 removers.push(injectGlobalCss(color.cssText, '', target, true));
 removers.push(injectGlobalCss(spacing.cssText, '', target, true));
-removers.push(injectGlobalCss(badge.cssText, '', target, true));
 removers.push(injectGlobalCss(utility.cssText, '', target, true));
+removers.push(injectGlobalCss(badge.cssText, '', target, true));
 
       
     }
